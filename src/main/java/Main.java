46b7b8b11c88bb22010e07775478cfcd6c17fe8b
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 20:48:25 +0300

    Scripted auto-commit on change (2021-12-16 20:48:25) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
@@ -6,16 +7,18 @@ import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
+import akka.stream.javadsl.Flow;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println();
         ActorSystem system = ActorSystem.create();
-        ActorMaterializer materializer = new ActorMaterializer(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
         ActorRef configStore = system.actorOf(Props.create(ConfigurationStore.class));
         Http http = Http.get(system);
         HttpRoute httpRoute = new HttpRoute(configStore, http);
-        Flow<HttpRequest, HttpResponse, NotUsed> flowRoute = httpRoute.GetHttpRoute().flow(system)
+        Flow<HttpRequest, HttpResponse, NotUsed> flowRoute = httpRoute.GetHttpRoute().flow(system, materializer);
+        
 
     }
 }
