f95db41f3cd08bdd8734809053766369911a8a67
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 22:32:56 +0300

    Scripted auto-commit on change (2021-12-16 22:32:56) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ import java.util.concurrent.CompletionStage;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181")
+        ZooKeeper zooKeeper = new ZooKeeper("localhost:2181", 2000, )
         ActorSystem system = ActorSystem.create();
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         ActorRef configStore = system.actorOf(Props.create(ConfigurationStore.class));
