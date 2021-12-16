703ab5daf8ff2b18921f946e2e4830bf925310dc
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 19:30:26 +0300

    Scripted auto-commit on change (2021-12-16 19:30:26) by gitwatch.sh

--- src/main/java/HttpRoute.java
+++ src/main/java/HttpRoute.java
@@ -4,11 +4,16 @@ import static akka.http.javadsl.server.Directives.*;
 
 public class HttpRoute {
 
-    
+    public static final String URL_PARAMETR = "url";
+    public static final String COUNT_PARAMETR = "count";
 
     public static Route GetHttpRoute() {
         return route(get(() ->
-                parameter()
+                parameter(URL_PARAMETR, url -> {
+                    parameter(COUNT_PARAMETR, count -> {
+                        
+                    });
+                })
                 )
         );
     }
