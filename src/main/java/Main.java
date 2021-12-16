5662667713674879546f816f625a27e6de58a4dc
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 20:49:21 +0300

    Scripted auto-commit on change (2021-12-16 20:49:21) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,20 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.Materializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println();
         ActorSystem system = ActorSystem.create();
         final ActorMaterializer materializer = ActorMaterializer.create(system);
@@ -18,7 +23,15 @@ public class Main {
         Http http = Http.get(system);
         HttpRoute httpRoute = new HttpRoute(configStore, http);
         Flow<HttpRequest, HttpResponse, NotUsed> flowRoute = httpRoute.GetHttpRoute().flow(system, materializer);
-        
-
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                flowRoute,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
