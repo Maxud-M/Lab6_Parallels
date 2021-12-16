7d54f78371964cf45896d9ca294f6a7f4d68e526
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 19:31:51 +0300

    Scripted auto-commit on change (2021-12-16 19:31:51) by gitwatch.sh

--- src/main/java/HttpRoute.java
+++ src/main/java/HttpRoute.java
@@ -1,8 +1,11 @@
+import akka.actor.ActorRef;
 import akka.http.javadsl.server.Route;
 
 import static akka.http.javadsl.server.Directives.*;
 
 public class HttpRoute {
+
+    ActorRef configStore;
 
     public static final String URL_PARAMETR = "url";
     public static final String COUNT_PARAMETR = "count";
@@ -11,7 +14,7 @@ public class HttpRoute {
         return route(get(() ->
                 parameter(URL_PARAMETR, url -> {
                     parameter(COUNT_PARAMETR, count -> {
-                        
+
                     });
                 })
                 )
