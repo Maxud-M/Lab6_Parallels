58b64584b82727429fa2d56e4441225a44fc35c7
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 23:33:32 +0300

    Scripted auto-commit on change (2021-12-16 23:33:32) by gitwatch.sh

--- src/main/java/ChildZNodeWatcher.java
+++ src/main/java/ChildZNodeWatcher.java
@@ -1,10 +1,11 @@
+import akka.actor.ActorRef;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class ChildZNodeWatcher implements Watcher {
 
 
-    ChildZNodeWatcher()
+    ChildZNodeWatcher(ActorRef configStore)
 
     @Override
     public void process(WatchedEvent watchedEvent) {
