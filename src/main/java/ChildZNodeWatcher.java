ecfb0e14afac1d27b66851f01827d2a26666bfb7
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 23:32:28 +0300

    Scripted auto-commit on change (2021-12-16 23:32:28) by gitwatch.sh

--- src/main/java/ChildZNodeWatcher.java
+++ src/main/java/ChildZNodeWatcher.java
@@ -1,2 +1,11 @@
-public class ChildZNodeWatcher implements {
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class ChildZNodeWatcher implements Watcher {
+    
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
