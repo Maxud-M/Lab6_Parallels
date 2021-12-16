ee155afb257365380719a9546adf82cb41af8b98
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 20:45:34 +0300

    Scripted auto-commit on change (2021-12-16 20:45:34) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println();
         ActorSystem system = ActorSystem.create();
         ActorRef storeActor = system.actorOf(Props.create(ConfigurationStore.class));
-        Http http = Http.get(system)
+        Http http = Http.get(system);
+        
 
     }
 }
