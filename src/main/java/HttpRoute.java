eda053d128ce143c7c1f760a1953bb2e31de26d3
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-16 19:57:53 +0300

    Scripted auto-commit on change (2021-12-16 19:57:53) by gitwatch.sh

--- src/main/java/HttpRoute.java
+++ src/main/java/HttpRoute.java
@@ -7,7 +7,6 @@ import akka.pattern.Patterns;
 import java.time.Duration;
 
 import static akka.http.javadsl.server.Directives.*;
-import static org.asynchttpclient.Dsl.asyncHttpClient;
 import static org.asynchttpclient.Dsl.get;
 
 public class HttpRoute {
@@ -25,7 +24,7 @@ public class HttpRoute {
     public static final String COUNT_PARAMETR = "count";
 
     public Route GetHttpRoute() {
-        return route(get(() ->
+        return route((get(() ->
                 parameter(URL_PARAMETR, url -> {
                     parameter(COUNT_PARAMETR, countStr -> {
                         int count = Integer.parseInt(countStr);
